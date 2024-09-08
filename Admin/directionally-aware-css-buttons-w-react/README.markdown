5d9a4558b46bdf14c0393b5b76b9ed5b6140e937
Ishan-2003 <21BCS022@nith.ac.in>
2024-09-08 11:30:35 +0530

    day -6

--- Admin/directionally-aware-css-buttons-w-react/README.markdown
+++
@@ -1,5 +0,0 @@
-# Directionally Aware CSS Buttons w/ React
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/jh3y/pen/XWMobqx](https://codepen.io/jh3y/pen/XWMobqx).
-
-
--- Frontend/ecommerce-frontend/src/features/counter/pages/Ourstore.js
+++ Frontend/ecommerce-frontend/src/features/counter/pages/Ourstore.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Template from '../Components/Template';
 import MetaTitle from '../Components/MetaTitle';
 import Slider from '@mui/material/Slider';
+import { FaGripLinesVertical } from "react-icons/fa";
+import { TbMinusVertical } from "react-icons/tb";
+import { RxDragHandleVertical } from "react-icons/rx";
+import { MdHorizontalSplit } from "react-icons/md";
+import ProductCard from '../Components/ProductCard';
 
 function valuetext(value) {
     return `${value}°C`;
 }
 
 const Ourstore = () => {
+    const [grid, setgrid] = useState(4);
+    const gridsetMethod = async (numCols) => {
+        setgrid(numCols);
+    }
+    // if(gridsetMethod) alert(grid)
     return (
         <>
             <MetaTitle title="My Store" />
@@ -68,10 +78,38 @@ const Ourstore = () => {
                     <div>Product Tags</div>
                     <div>Arbitrary Product Filter</div>
                 </div>
-                <div className='store-result-area'>
-                    <h3>
-                        Product-area
-                    </h3>
+                <div className='store-res-wrapper'>
+                    <div className='sort-by-nav'>
+                        <div className='sort-by'>
+                            <p>Sort By:</p>
+                            <select name="" id="prod-sort">
+                                <option value="manual">Customised</option>
+                                <option value="best-selling" selected>Best Selling</option>
+                                <option value="alpha-asc">A-Z</option>
+                                <option value="alpha-desc">Z-A</option>
+                                <option value="price-inc">Price (0 - inf)</option>
+                                <option value="price-dec">Price (inf - 0)</option>
+                                <option value="old">Date newest</option>
+                                <option value="new">Date old</option>
+                            </select>
+                        </div>
+                        <div className='display-setting'>
+                            <p>22 products</p>
+                            <div>
+                                <RxDragHandleVertical size={31} onClick={() => gridsetMethod(4)} />
+                                <FaGripLinesVertical size={30} onClick={() => gridsetMethod(2)} />
+                                <TbMinusVertical size={31} onClick={() => gridsetMethod(1)} />
+                                <MdHorizontalSplit size={31} onClick={() => gridsetMethod(1)} />
+                            </div> 
+                        </div>
+                    </div>
+                    <div className='store-result-area p-4 d-grid' style={{gridTemplateColumns:`repeat(${grid},1fr)`}}>
+                            <ProductCard />
+                            <ProductCard />
+                            <ProductCard />
+                            <ProductCard />
+                    </div>
+
                 </div>
             </div>
         </>
